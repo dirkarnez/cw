91d111971054d8be392800876f73090ecfd4deb9
dirkarnez <smalldirkalex@gmail.com>
2020-07-20 12:48:00 +0800

    - update main.cpp, settings.json

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -8,14 +8,14 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <map>
 
 #include <boost/format.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
 #include <inputbox.h>
-//#include <lua/lua.h>
-
+//https://cpp.hotexamples.com/examples/-/-/luaL_loadstring/cpp-lual_loadstring-function-examples.html
 #include "foo_bar.h"
 
 using namespace std;
@@ -23,107 +23,95 @@ using namespace boost;
 
 class param {
 public:
-	char* display_name;
-	char* cli_argument;
-	char* default_value;
-	char* description;
-
-	param(char* display_name, char* default_value, char* description = (char*)"") {
-		this->display_name = display_name;
-		this->default_value = default_value;
-		this->description = description;
-	}
+	param() = default;
+	string display_name;
+	string default_value;
+	string description;
 
 	string prompt() {
-		return string(dependency_test::lib_a::InputBox(description, display_name, default_value));
+		return string(dependency_test::lib_a::InputBox(
+			const_cast<char*>(description.c_str()), 
+			const_cast<char*>(display_name.c_str()), 
+			const_cast<char*>(default_value.c_str())));
 	}
 };
 
 //https://stackoverflow.com/questions/38053233/c-why-did-boost-authors-use-a-struct-here-instead-of-a-class/38053913
 //MsgBox("Hello World!", 3, "MsgBox Example")
-//docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password mariadb
 class command {
 public:
 	command() = default;
-	string line;
+	string name;
+	string line;  //command
 	std::vector<param> param_list;
 
 	string print() {
 		format fmter(line);
 		for (param param : this->param_list) {
-			fmter = fmter % param.prompt();
+			string input = param.prompt();
+			if (input.empty()) {
+				return std::string();
+			}
+			fmter = fmter % input;
 		}
 		return fmter.str();
-	}
-
-	void load(const std::string& filename) {
-		// Create empty property tree object
-		boost::property_tree::ptree tree;
-
-		// Parse the XML into the property tree.
-		boost::property_tree::json_parser::read_json(filename, tree);
-
-		this->line = tree.get<string>("command");
-
-		//// Use the default-value version of get to find the debug level.
-		//// Note that the default value is used to deduce the target type.
-		//m_level = tree.get("debug.level", 0);
-
-		//// Use get_child to find the node containing the modules, and iterate over
-		//// its children. If the path cannot be resolved, get_child throws.
-		//// A C++11 for-range loop would also work.
-
 	}
 };
 
 class setting {
-	std::vector<command> command_list;
+	std::map<string, command> command_list;
 
+public:
 	void load(const std::string& filename) {
-		// Create empty property tree object
 		boost::property_tree::ptree tree;
 
-		// Parse the XML into the property tree.
-		boost::property_tree::json_parser::read_json(filename, tree);
+		boost::property_tree::json_parser::read_json(filename, tree, std::locale("en_US.UTF-8"));
 
-		BOOST_FOREACH(boost::property_tree::ptree::value_type &v, tree.get_child("debug.modules")) {
-			cout << v.second.data();
+		BOOST_FOREACH(boost::property_tree::ptree::value_type &v, tree) {
+			auto node = v.second;
+			command c;
+			c.name = node.get<string>("name");
+			c.line = node.get<string>("command");
+			c.param_list = {};
+
+			BOOST_FOREACH(boost::property_tree::ptree::value_type &parameter, node.get_child("parameters"))
+			{
+				auto parameter_node = parameter.second;
+				param p;
+				p.default_value = parameter_node.get<string>("default");
+				p.display_name = parameter_node.get<string>("displayName");
+				p.description = parameter_node.get<string>("description");
+				c.param_list.push_back(p);
+			}
+			command_list.insert(pair<string, command>(c.name, c));
+		}
+	}
+
+	void get(_TCHAR* arg) {
+		string cl = command_list[arg].print();
+		if (cl.empty()) {
+			cout << "Invalid input" << endl;
+			system("pause");
+		}
+		else {
+			cout << cl << endl;
+			system(cl.c_str());
 		}
 	}
 };
 
 #if defined(WIN32)
-int _tmain(int /*argc*/, _TCHAR* /*argv*/[])
+int _tmain(int /*argc*/, _TCHAR* argv[])
 #else
 int main(int /*argc*/, char* /*argv*/[])
 #endif
 {
   try
   {
-    //dependency_test::app::foo();
-    //dependency_test::app::bar();
-	//docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password mariadb
-	/**
-	{
-		"command": "docker run -d -p %1%:3306 -e MYSQL_ROOT_PASSWORD=%2% mariadb",
-		"parameters": [
-			{
-				"default": 3306
-				"display_name": "port",
-				"description": ""
-			}
-		]
-	}
-	**/
-
-	  command c;
-	  c.load("settings.json");
-	  c.param_list = { param("port", "3306") };
-	  string cl = c.print();
-	  cout << cl << endl;
-
-	 system(cl.c_str());
-    return EXIT_SUCCESS;
+	  setting s;
+	  s.load("settings.json");
+	  s.get(argv[1]);
+	  return EXIT_SUCCESS;
   }
   catch (const std::exception& e)
   {
