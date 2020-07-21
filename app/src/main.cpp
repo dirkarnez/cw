cfa6fe860b70b22988a4a09c880e2619f388084b
dirkarnez <smalldirkalex@gmail.com>
2020-07-21 19:33:19 +0800

    - remove boost process environment, add cw-scripts

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -17,16 +17,13 @@
 #include <boost/format.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <boost/process.hpp>
-#include <boost/process/env.hpp>
-#include <boost/process/environment.hpp>
-#include <boost/process/system.hpp>
 #include <boost/foreach.hpp>
 #include <inputbox.h>
-//https://cpp.hotexamples.com/examples/-/-/luaL_loadstring/cpp-lual_loadstring-function-examples.html
 #include "foo_bar.h"
 #include <lua.hpp>
 #include <lauxlib.h>
+#include <boost\filesystem\path.hpp>
+#include <boost\filesystem\operations.hpp>
 
 using namespace std;
 using namespace boost;
@@ -49,13 +46,12 @@ public:
 	string evaluate;
 
 	std::vector<Variable> variable_list;
-	
-	// https://www.lua.org/manual/5.1/manual.html
-	int eval(string expression) {
+
+	auto eval(string expression) {
 		lua_State* L = luaL_newstate();
 		luaL_openlibs(L);
 		luaL_dostring(L, expression.c_str());
-		int temp = lua_tointeger(L, 1);
+		auto temp = lua_tointeger(L, 1);
 		lua_close(L);
 		return temp;
 	}
@@ -77,15 +73,13 @@ public:
 			}
 			expression_stream << "return " << evaluate;
 			string expression = expression_stream.str();
-			int result = eval(expression);
 			stringstream ss;
-			ss << result;
+			ss << eval(expression);
 			return ss.str();
 		}
 	}
 };
 
-//https://stackoverflow.com/questions/38053233/c-why-did-boost-authors-use-a-struct-here-instead-of-a-class/38053913
 //MsgBox("Hello World!", 3, "MsgBox Example")
 class command {
 public:
@@ -112,50 +106,48 @@ class setting {
 	map<string, command> command_list;
 
 public:
-	void load(const std::string& filename) {
-		boost::property_tree::ptree tree;
+	void add(const std::string& filename) {
+		using namespace boost::property_tree;
 
-		boost::property_tree::json_parser::read_json(filename, tree, std::locale("en_US.UTF-8"));
+		ptree tree;
+		json_parser::read_json(filename, tree);
 
-		BOOST_FOREACH(boost::property_tree::ptree::value_type &v, tree) {
-			auto node = v.second;
-			command c;
-			c.name = node.get<string>("name");
-			c.line = node.get<string>("command");
-			c.param_list = {};
+		command c;
+		c.name = tree.get<string>("name");
+		c.line = tree.get<string>("command");
+		c.param_list = {};
 
-			BOOST_FOREACH(boost::property_tree::ptree::value_type &parameter, node.get_child("parameters"))
+		BOOST_FOREACH(boost::property_tree::ptree::value_type &parameter, tree.get_child("parameters"))
+		{
+			auto parameter_node = parameter.second;
+			param p;
+			p.variable_list = {};
+
+			if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
 			{
-				auto parameter_node = parameter.second;
-				param p;
-				p.variable_list = {};
-				
-				if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
-				{
-					p.evaluate = evaluate.get();
-				}
-				else {
-					p.default_value = parameter_node.get<string>("default");
-					p.display_name = parameter_node.get<string>("displayName");
-					p.description = parameter_node.get<string>("description");
-				}
-
-				if (!p.evaluate.empty()) {
-					BOOST_FOREACH(boost::property_tree::ptree::value_type &variable, parameter_node.get_child("variables"))
-					{
-						auto variable_node = variable.second;
-						Variable v;
-						v.name = variable_node.get<string>("name");
-						v.default_value = variable_node.get<string>("default");
-						v.display_name = variable_node.get<string>("displayName");
-						v.description = variable_node.get<string>("description");
-						p.variable_list.push_back(v);
-					}
-				}
-				c.param_list.push_back(p);
+				p.evaluate = evaluate.get();
 			}
-			command_list.insert(pair<string, command>(c.name, c));
+			else {
+				p.default_value = parameter_node.get<string>("default");
+				p.display_name = parameter_node.get<string>("displayName");
+				p.description = parameter_node.get<string>("description");
+			}
+
+			if (!p.evaluate.empty()) {
+				BOOST_FOREACH(boost::property_tree::ptree::value_type & variable, parameter_node.get_child("variables"))
+				{
+					auto variable_node = variable.second;
+					Variable v;
+					v.name = variable_node.get<string>("name");
+					v.default_value = variable_node.get<string>("default");
+					v.display_name = variable_node.get<string>("displayName");
+					v.description = variable_node.get<string>("description");
+					p.variable_list.push_back(v);
+				}
+			}
+			c.param_list.push_back(p);
 		}
+		command_list.insert(pair<string, command>(c.name, c));
 	}
 
 	void get(_TCHAR* arg) {
@@ -179,15 +171,36 @@ int main(int /*argc*/, char* /*argv*/[])
 {
   try
   {
-	  //setting s;
-	  //s.load("settings.json");
-	  //s.get(argv[1]);
+	  setting s;
 
-	  auto env = boost::this_process::environment();
-	  env["PATH"] = "D:\\mingw64\\bin";
-	  boost::process::system("cmd", env);
-	  //setenv("PATH", ", 0);
+	  using namespace boost::filesystem;
+	  path p("cw-scripts");
+	  try
+	  {
+		  if (exists(p))
+		  {
+			  if (is_directory(p)) {
+				  for (auto& _p : recursive_directory_iterator(p)) {
+					  if (!is_directory(_p) && _p.path().extension() == ".cw") {
+						  s.add(_p.path().string());
+					  }
+				  }
+			  }
+			  else {
+				cout << p << " exists, but is not a  directory\n";
+			  }
+		  }
+		  else {
+			cout << p << " does not exist\n";
+		  }
+			 
+	  }
+	  catch (const filesystem_error& ex)
+	  {
+		  cout << ex.what() << '\n';
+	  }
 
+	  s.get(argv[1]);
 
 	  return EXIT_SUCCESS;
   }
