46d938ad6cda814b1220bfa144ab0e8f5d9c2260
dirkarnez <smalldirkalex@gmail.com>
2020-07-28 16:05:21 +0800

    - update .cw structure

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -18,14 +18,17 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/foreach.hpp>
+#include <boost/algorithm/string/replace.hpp>
 #include <inputbox.h>
 #include "foo_bar.h"
 #include <lua.hpp>
 #include <lauxlib.h>
 #include <boost\filesystem\path.hpp>
 #include <boost\filesystem\operations.hpp>
-#include <boost/dll/runtime_symbol_info.hpp>
 
+
+//#include <boost/process/environment.hpp>
+#include <boost\dll\runtime_symbol_info.hpp>
 using namespace std;
 using namespace boost;
 
@@ -38,9 +41,9 @@ public:
 	string description;
 };
 
-class param {
+class Param {
 public:
-	param() = default;
+	Param() = default;
 	string display_name;
 	string default_value;
 	string description;
@@ -81,20 +84,18 @@ public:
 	}
 };
 
-//MsgBox("Hello World!", 3, "MsgBox Example")
-class command {
+class Command {
 public:
-	command() = default;
-	string name;
-	string line;  //command
-	std::vector<param> param_list;
+	Command() = default;
+	string line;
+	vector<Param> param_list;
 
 	string print() {
 		format fmter(line);
-		for (param param : this->param_list) {
+		for (Param param : this->param_list) {
 			string input = param.prompt();
 			if (input.empty()) {
-				return std::string();
+				return string();
 			}
 			fmter = fmter % input;
 		}
@@ -103,63 +104,89 @@ public:
 	}
 };
 
-class setting {
-	map<string, command> command_list;
-
+//MsgBox("Hello World!", 3, "MsgBox Example")
+// Per JSON
+class WrappedCommand {
 public:
-	void add(const std::string& filename) {
+	WrappedCommand() = default;
+	string name;
+	vector<Command> command_list;
+};
+
+class Setting {
+public:
+	map<string, WrappedCommand> wrapped_command_list;
+
+	void add(const boost::filesystem::path& path) {
 		using namespace boost::property_tree;
 
 		ptree tree;
-		json_parser::read_json(filename, tree);
+		json_parser::read_json(path.string(), tree);
 
-		command c;
-		c.name = tree.get<string>("name");
-		c.line = tree.get<string>("command");
-		c.param_list = {};
+		WrappedCommand wc;
+		wc.name = tree.get<string>("name");
+		wc.command_list = {};
 
-		BOOST_FOREACH(boost::property_tree::ptree::value_type &parameter, tree.get_child("parameters"))
+		BOOST_FOREACH(boost::property_tree::ptree::value_type & command_value, tree.get_child("commands"))
 		{
-			auto parameter_node = parameter.second;
-			param p;
-			p.variable_list = {};
+			auto command_node = command_value.second;
+			Command command;
+			command.line = command_node.get<string>("command");
+	
+			boost::optional<ptree&> optional_parameter_value = command_node.get_child_optional("parameters");
 
-			if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
-			{
-				p.evaluate = evaluate.get();
-			}
-			else {
-				p.default_value = parameter_node.get<string>("default");
-				p.display_name = parameter_node.get<string>("displayName");
-				p.description = parameter_node.get<string>("description");
-			}
-
-			if (!p.evaluate.empty()) {
-				BOOST_FOREACH(boost::property_tree::ptree::value_type & variable, parameter_node.get_child("variables"))
+			if (optional_parameter_value.has_value()) {
+				BOOST_FOREACH(boost::property_tree::ptree::value_type & parameter_value, optional_parameter_value.get())
 				{
-					auto variable_node = variable.second;
-					Variable v;
-					v.name = variable_node.get<string>("name");
-					v.default_value = variable_node.get<string>("default");
-					v.display_name = variable_node.get<string>("displayName");
-					v.description = variable_node.get<string>("description");
-					p.variable_list.push_back(v);
+					auto parameter_node = parameter_value.second;
+					Param p;
+					p.variable_list = {};
+
+					if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
+					{
+						p.evaluate = evaluate.get();
+					}
+					else {
+						p.display_name = parameter_node.get<string>("displayName");
+						p.description = parameter_node.get<string>("description");
+						p.default_value = parameter_node.get<string>("default");
+						boost::replace_all(p.default_value, "{{SCRIPT_LOCATION}}", path.branch_path().string()); 
+						// buggy, see https://stackoverflow.com/questions/34466077/how-to-implement-escaping
+					}
+
+					if (!p.evaluate.empty()) {
+						BOOST_FOREACH(boost::property_tree::ptree::value_type & variable, parameter_node.get_child("variables"))
+						{
+							auto variable_node = variable.second;
+							Variable v;
+							v.name = variable_node.get<string>("name");
+							v.default_value = variable_node.get<string>("default");
+							v.display_name = variable_node.get<string>("displayName");
+							v.description = variable_node.get<string>("description");
+							p.variable_list.push_back(v);
+						}
+					}
+					command.param_list.push_back(p);
 				}
 			}
-			c.param_list.push_back(p);
+			
+			wc.command_list.push_back(command);
 		}
-		command_list.insert(pair<string, command>(c.name, c));
+
+		wrapped_command_list.insert(pair<string, WrappedCommand>(wc.name, wc));
 	}
 
 	void get(_TCHAR* arg) {
-		string cl = command_list[arg].print();
-		if (cl.empty()) {
-			cout << "Invalid input" << endl;
-			std::system("pause");
-		}
-		else {
-			cout << cl << endl;
-			std::system(cl.c_str());
+		for (Command command : this->wrapped_command_list[arg].command_list) {
+			string cl = command.print();
+			if (cl.empty()) {
+				cout << "Invalid input" << endl;
+				std::system("pause");
+			}
+			else {
+				cout << cl << endl;
+				std::system(cl.c_str());
+			}
 		}
 	}
 };
@@ -170,9 +197,13 @@ int _tmain(int /*argc*/, _TCHAR* argv[])
 int main(int /*argc*/, char* /*argv*/[])
 #endif
 {
+	//_putenv(string("PATH=").append(getenv("PATH")).append("c:\\mylib;c:\\yourlib").c_str());
+
+	//std::system("C:\\windows\\System32\\cmd.exe");
+
   try
   {
-	  setting s;
+	  Setting s;
 
 	  using namespace boost::filesystem;
 	  
@@ -184,7 +215,7 @@ int main(int /*argc*/, char* /*argv*/[])
 			  if (is_directory(p)) {
 				  for (auto& _p : recursive_directory_iterator(p)) {
 					  if (!is_directory(_p) && _p.path().extension() == ".cw") {
-						  s.add(_p.path().string());
+						  s.add(_p.path());
 					  }
 				  }
 			  }
@@ -195,7 +226,6 @@ int main(int /*argc*/, char* /*argv*/[])
 		  else {
 			cout << p << " does not exist\n";
 		  }
-			 
 	  }
 	  catch (const filesystem_error& ex)
 	  {
