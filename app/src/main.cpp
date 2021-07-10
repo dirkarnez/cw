c524a9ceba71d1a41339a7e8dd221804f6c2f521
dirkarnez <smalldirkalex@gmail.com>
2021-07-10 09:01:14 +0800

    - use yaml-cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -23,21 +23,14 @@
 #include "foo_bar.h"
 #include <lua.hpp>
 #include <lauxlib.h>
-#include <boost\filesystem\path.hpp>
-#include <boost\filesystem\operations.hpp>
-
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
 
 //#include <boost/process/environment.hpp>
-#include <boost\dll\runtime_symbol_info.hpp>
+#include <boost/dll/runtime_symbol_info.hpp>
 
 #include <yaml-cpp/yaml.h>
 #include <yaml-cpp/node/node.h>
-
-
-
-
-
-
 
 
 #include <algorithm>
@@ -45,6 +38,7 @@
 #include <elements.hpp>
 
 using namespace cycfi::elements;
+using namespace boost::filesystem;
 
 // Main window background color
 auto constexpr bkd_color = rgba(35, 35, 37, 255);
@@ -254,7 +248,6 @@ public:
 
 class Command {
 public:
-	Command() = default;
 	string line;
 	vector<Param> param_list;
 
@@ -276,87 +269,153 @@ public:
 // Per JSON
 class WrappedCommand {
 public:
-	WrappedCommand() = default;
 	string name;
 	vector<Command> command_list;
 };
 
 class Setting {
 public:
-	map<string, WrappedCommand> wrapped_command_list;
-
-	void add(const boost::filesystem::path& path) {
-		using namespace boost::property_tree;
-
-		ptree tree;
-		json_parser::read_json(path.string(), tree);
-
-		WrappedCommand wc;
-		wc.name = tree.get<string>("name");
-		wc.command_list = {};
-
-		BOOST_FOREACH(boost::property_tree::ptree::value_type & command_value, tree.get_child("commands"))
+	Setting(path p) {
+		try
 		{
-			auto command_node = command_value.second;
-			Command command;
-			command.line = command_node.get<string>("command");
-	
-			boost::optional<ptree&> optional_parameter_value = command_node.get_child_optional("parameters");
-
-			if (optional_parameter_value.has_value()) {
-				BOOST_FOREACH(boost::property_tree::ptree::value_type & parameter_value, optional_parameter_value.get())
-				{
-					auto parameter_node = parameter_value.second;
-					Param p;
-					p.variable_list = {};
-
-					if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
-					{
-						p.evaluate = evaluate.get();
-					}
-					else {
-						p.display_name = parameter_node.get<string>("displayName");
-						p.description = parameter_node.get<string>("description");
-						p.default_value = parameter_node.get<string>("default");
-						boost::replace_all(p.default_value, "{{SCRIPT_LOCATION}}", path.branch_path().string()); 
-						// buggy, see https://stackoverflow.com/questions/34466077/how-to-implement-escaping
-					}
-
-					if (!p.evaluate.empty()) {
-						BOOST_FOREACH(boost::property_tree::ptree::value_type & variable, parameter_node.get_child("variables"))
-						{
-							auto variable_node = variable.second;
-							Variable v;
-							v.name = variable_node.get<string>("name");
-							v.default_value = variable_node.get<string>("default");
-							v.display_name = variable_node.get<string>("displayName");
-							v.description = variable_node.get<string>("description");
-							p.variable_list.push_back(v);
+			if (exists(p))
+			{
+				if (is_directory(p)) {
+					for (auto& _p : recursive_directory_iterator(p)) {
+						if (!is_directory(_p) && _p.path().extension() == ".yml") {
+							add(_p.path());
 						}
 					}
-					command.param_list.push_back(p);
+				}
+				else {
+					cout << p << " exists, but is not a directory\n";
 				}
 			}
-			
-			wc.command_list.push_back(command);
+			else {
+				cout << p << " does not exist\n";
+			}
+		}
+		catch (const filesystem_error& ex)
+		{
+			cout << ex.what() << '\n';
+		}
+	}
+private:
+	//map<string, WrappedCommand> wrapped_command_list;
+
+	void add(path p) {
+		using namespace YAML;
+
+		Node fileNode = LoadFile(p.string());
+
+		Node nameNode = fileNode["name"];
+		if (nameNode) {
+			cout << "name: " << nameNode.as<string>() << endl;
 		}
 
-		wrapped_command_list.insert(pair<string, WrappedCommand>(wc.name, wc));
+		Node commandsNode = fileNode["commands"];
+
+		if (commandsNode) {
+			vector<Node> commands = commandsNode.as<vector<Node>>();
+
+			for (int i = 0; i < commands.size(); i++) {
+				Node commandNode = commands.at(i);
+
+				Node commandCommandNode = commandNode["command"];
+
+				if (commandCommandNode) {
+					cout << "command: " << commandCommandNode.as<string>() << endl;
+				}
+
+				vector<Node> parameters = commandNode["parameters"].as<vector<Node>>();
+				for (int j = 0; j < parameters.size(); j++) {
+					Node parameterNode = parameters.at(j);
+
+					cout << "default: " << parameterNode["default"].as<string>() << endl;
+					cout << "displayName: " << parameterNode["displayName"].as<string>() << endl;
+					cout << "description: " << parameterNode["description"].as<string>() << endl;
+				}
+			}
+		}
 	}
 
-	void get(_TCHAR* arg) {
-		//for (Command command : this->wrapped_command_list[arg].command_list) {
-		//	string cl = command.print();
-		//	if (cl.empty()) {
-		//		cout << "Invalid input" << endl;
-		//		std::system("pause");
-		//	}
-		//	else {
-		//		cout << cl << endl;
-		//		std::system(cl.c_str());
-		//	}
-		//}
-	}
+
+//
+//public:
+//	map<string, WrappedCommand> wrapped_command_list;
+//
+//	void add(const boost::filesystem::path& path) {
+//		using namespace boost::property_tree;
+//
+//		ptree tree;
+//		json_parser::read_json(path.string(), tree);
+//
+//		WrappedCommand wc;
+//		wc.name = tree.get<string>("name");
+//		wc.command_list = {};
+//
+//		BOOST_FOREACH(boost::property_tree::ptree::value_type & command_value, tree.get_child("commands"))
+//		{
+//			auto command_node = command_value.second;
+//			Command command;
+//			command.line = command_node.get<string>("command");
+//	
+//			boost::optional<ptree&> optional_parameter_value = command_node.get_child_optional("parameters");
+//
+//			if (optional_parameter_value.has_value()) {
+//				BOOST_FOREACH(boost::property_tree::ptree::value_type & parameter_value, optional_parameter_value.get())
+//				{
+//					auto parameter_node = parameter_value.second;
+//					Param p;
+//					p.variable_list = {};
+//
+//					if (boost::optional<string> evaluate = parameter_node.get_optional<string>("evaluate"))
+//					{
+//						p.evaluate = evaluate.get();
+//					}
+//					else {
+//						p.display_name = parameter_node.get<string>("displayName");
+//						p.description = parameter_node.get<string>("description");
+//						p.default_value = parameter_node.get<string>("default");
+//						boost::replace_all(p.default_value, "{{SCRIPT_LOCATION}}", path.branch_path().string()); 
+//						// buggy, see https://stackoverflow.com/questions/34466077/how-to-implement-escaping
+//					}
+//
+//					if (!p.evaluate.empty()) {
+//						BOOST_FOREACH(boost::property_tree::ptree::value_type & variable, parameter_node.get_child("variables"))
+//						{
+//							auto variable_node = variable.second;
+//							Variable v;
+//							v.name = variable_node.get<string>("name");
+//							v.default_value = variable_node.get<string>("default");
+//							v.display_name = variable_node.get<string>("displayName");
+//							v.description = variable_node.get<string>("description");
+//							p.variable_list.push_back(v);
+//						}
+//					}
+//					command.param_list.push_back(p);
+//				}
+//			}
+//			
+//			wc.command_list.push_back(command);
+//		}
+//
+//		wrapped_command_list.insert(pair<string, WrappedCommand>(wc.name, wc));
+//	}
+//
+//	void get(_TCHAR* arg) {
+//		//for (Command command : this->wrapped_command_list[arg].command_list) {
+//		//	string cl = command.print();
+//		//	if (cl.empty()) {
+//		//		cout << "Invalid input" << endl;
+//		//		std::system("pause");
+//		//	}
+//		//	else {
+//		//		cout << cl << endl;
+//		//		std::system(cl.c_str());
+//		//	}
+//		//}
+//	}
 };
 
 //#if defined(WIN32)
@@ -377,61 +436,41 @@ int main(int argc, char* argv[])
 	//	cout << "name: " << config["name"].as<string>() << "\n";
 	//}
 
-	app _app(argc, argv, "Buttons", "com.cycfi.buttons");
-	window _win(_app.name());
-	_win.on_close = [&_app]() { _app.stop(); };
+	//app _app(argc, argv, "Buttons", "com.cycfi.buttons");
+	//window _win(_app.name());
+	//_win.on_close = [&_app]() { _app.stop(); };
 
-	view view_(_win);
+	//view view_(_win);
 
-	view_.content(
-		make_controls(view_),
-		background
-	);
+	//view_.content(
+	//	make_controls(view_),
+	//	background
+	//);
 
-	_app.run();
+	//_app.run();
 
-  //try
-  //{
-	 // Setting s;
+	Setting s(boost::dll::program_location().parent_path() / "cw-scripts");
 
-	 // using namespace boost::filesystem;
-	 // 
-	 // path p = boost::dll::program_location().parent_path() / "cw-scripts";
-	 // try
-	 // {
-		//  if (exists(p))
-		//  {
-		//	  if (is_directory(p)) {
-		//		  for (auto& _p : recursive_directory_iterator(p)) {
-		//			  if (!is_directory(_p) && _p.path().extension() == ".cw") {
-		//				  s.add(_p.path());
-		//			  }
-		//		  }
-		//	  }
-		//	  else {
-		//		cout << p << " exists, but is not a directory\n";
-		//	  }
-		//  }
-		//  else {
-		//	cout << p << " does not exist\n";
-		//  }
-	 // }
-	 // catch (const filesystem_error& ex)
-	 // {
-		//  cout << ex.what() << '\n';
-	 // }
+
+  try
+  {
+
+
+
+	  
+	 
 
 	 // s.get(argv[1]);
 
-	 // return EXIT_SUCCESS;
-  //}
-  //catch (const std::exception& e)
-  //{
-  //  cerr << "Error: " << e.what() << endl;
-  //}
-  //catch (...)
-  //{
-  //  cerr << "Unexpected error" << endl;
-  //}
-  //return EXIT_FAILURE;
+	 return EXIT_SUCCESS;
+  }
+  catch (const std::exception& e)
+  {
+    cerr << "Error: " << e.what() << endl;
+  }
+  catch (...)
+  {
+    cerr << "Unexpected error" << endl;
+  }
+  return EXIT_FAILURE;
 }
